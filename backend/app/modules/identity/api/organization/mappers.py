437d241228c6c4f72a4a3d4e057d59d496d6e22a
mustafa sinan koca <mustafasinankoca@gmail.com>
2026-08-17 02:38:16 +0300

    Remove membership from organization API mapper

--- backend/app/modules/identity/api/organization/mappers.py
+++ backend/app/modules/identity/api/organization/mappers.py
@@ -81,5 +81,4 @@ def create_organization_result_to_response(
 ) -> CreateOrganizationResponse:
     return CreateOrganizationResponse(
         organization=organization_result_to_response(result.organization),
-        membership_id=result.membership_id.value if result.membership_id is not None else None,
     )
